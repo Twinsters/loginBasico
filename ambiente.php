cd775e2e39d5ccd475a282a34ec66a80035f8cd0
Claw <claudio_eh@hotmail.com>
2025-01-22 22:06:35 -0300

    Sexta Version

--- ambiente.php
+++ ambiente.php
@@ -1,14 +1,7 @@
 <?php
  
-//DNS de la PC
+    $USER = "";
+    $PASS = "";
+    $DNS = "sqlsrv:Server=[Nombre del servidor]; Database=[Nombre de la Base]";$USER;$PASS;
 
-$USER = "DESKTOP-KMBJ0IJ\Claw";
-$PASS = "";
-$DNS = "sqlsrv:Server=DESKTOP-KMBJ0IJ\SQLEXPRESS; Database=MiBaseListas";$USER;$PASS;
-/*
-//DNS de la NoteBook
-$USER = "DESKTOP-LIBA4PV\Claw";
-$PASS = "";
-$DNS = "sqlsrv:Server=DESKTOP-LIBA4PV\SQLEXPRESS; Database=MiBaseListas";$USER;$PASS;
-*/
 ?>
