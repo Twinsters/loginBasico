b9758a39fb7db740f0836c8b3900f2ab50ebd5e6
Claw <claudio_eh@hotmail.com>
2025-01-23 09:28:44 -0300

    Sexta Version

--- ambiente.php
+++ ambiente.php
@@ -1,11 +1,9 @@
 <?php
- /*
+ 
     $USER = "";
     $PASS = "";
     $DNS = "sqlsrv:Server=[Nombre del servidor]; Database=[Nombre de la Base]";$USER;$PASS;
-*/
 
-$USER = "DESKTOP-LIBA4PV\Claw";
-$PASS = "";
-$DNS = "sqlsrv:server=DESKTOP-LIBA4PV\SQLEXPRESS;Database=MiBaseListas";$USER;$PASS;
+
+
 ?>
